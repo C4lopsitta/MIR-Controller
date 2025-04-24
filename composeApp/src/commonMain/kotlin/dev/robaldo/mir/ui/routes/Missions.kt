67c6d7e4603c65722b05feac1acd28615a523fe2
c4lopsitta <simone@atomtech.cc>
2025-04-24 13:19:19 +0200

    Refactor: Added Pull-to-Refresh and Empty List Indicator
    - Added a pull-to-refresh functionality for the missions list.
    - Implemented an empty list indicator when no missions are available.
    - Modified UI for a better user experience.
    - Added `ButtonDefaults` import and fixed text alignment.

--- composeApp/src/commonMain/kotlin/dev/robaldo/mir/ui/routes/Missions.kt
+++ composeApp/src/commonMain/kotlin/dev/robaldo/mir/ui/routes/Missions.kt
@@ -8,13 +8,13 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.rounded.Add
 import androidx.compose.material.icons.rounded.Edit
 import androidx.compose.material.icons.rounded.Queue
-import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material.icons.rounded.SwipeDownAlt
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -23,21 +23,20 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.em
 import androidx.compose.ui.zIndex
 import androidx.navigation.NavHostController
 import dev.materii.pullrefresh.PullRefreshIndicator
 import dev.materii.pullrefresh.PullRefreshLayout
 import dev.materii.pullrefresh.rememberPullRefreshState
 import dev.robaldo.mir.api.MirApi
-import dev.robaldo.mir.models.responses.get.Item
 import dev.robaldo.mir.models.view.BotMissionsViewModel
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.IO
 import kotlinx.coroutines.launch
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.sp
 
 /**
  * The Missions route composable body.
@@ -111,9 +110,40 @@ fun Missions(
                 )
                 HorizontalDivider()
             }
-            item {
-                Box( modifier = Modifier.fillMaxWidth().height(64.dp) )
+            if(missionsViewModel.missions.value.isEmpty()) {
+                item {
+                    Column (
+                        verticalArrangement = Arrangement.Center,
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                        modifier = Modifier.fillMaxSize()
+                    ) {
+                        Icon(
+                            Icons.Rounded.SwipeDownAlt,
+                            contentDescription = "Icon",
+                            tint = MaterialTheme.colorScheme.onSurface,
+                            modifier = Modifier.width(92.dp).height(92.dp)
+                        )
+                        Text(
+                            "Pull to Refresh",
+                            color = MaterialTheme.colorScheme.onSurface,
+                            modifier = Modifier.padding( horizontal = 32.dp, vertical = 12.dp ),
+                            fontSize = 24.sp,
+                            textAlign = TextAlign.Center
+                        )
+                        Text(
+                            "Try to refresh the missions list.",
+                            color = MaterialTheme.colorScheme.onSurface,
+                            modifier = Modifier.padding( horizontal = 32.dp, vertical = 8.dp ),
+                            textAlign = TextAlign.Center
+                        )
+                    }
+                }
+            } else {
+                item {
+                    Box(modifier = Modifier.fillMaxWidth().height(64.dp))
+                }
             }
         }
     }
 }
+
