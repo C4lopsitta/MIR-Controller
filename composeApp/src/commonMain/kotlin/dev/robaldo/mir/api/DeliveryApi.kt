974a31bf4eb991ed9c2307ba1842d4eb196c7bf0
c4lopsitta <simone@atomtech.cc>
2025-05-08 08:18:56 +0200

    Initial creation of Delivery API file with stubs

---
+++ composeApp/src/commonMain/kotlin/dev/robaldo/mir/api/DeliveryApi.kt
@@ -0,0 +1,19 @@
+package dev.robaldo.mir.api
+
+object DeliveryApi {
+    suspend fun getInventory() {
+
+    }
+
+    suspend fun getClassrooms() {
+
+    }
+
+    suspend fun postOrder() {
+
+    }
+
+    suspend fun getOrders() {
+        
+    }
+}
